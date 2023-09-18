3c6adfa6f5ae45c9f8c5c8aa1f9dd4c4a996ccae
Ali-Usman3366 <aliusman3366503@gmail.com>
2023-09-18 19:16:11 +0500

    bootstrap removed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
 import ReactDOM from "react-dom";
 import Index from "./pages/Index";
 
